d4e11cb2d98de201f40a22e1e1ec565b3726f84a
Aditya Tapshalkar <aditya.taps@gatech.edu>
2021-02-07 00:08:41 -0500

    finished login page layout

---
+++ rushr-reactjs/src/components/login/Left.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Left.css'
+
+const Left = () => {
+    return(
+        <div className={"left-wrapper"}>
+            <div className={"left-text"}>
+                Smart insights for the <br/> fast-paced business.
+            </div>
+        </div>
+    )
+}
+
+export default Left
---
+++ rushr-reactjs/src/components/login/Sidebar.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import './Sidebar.css'
+import rushrLogo from '../assets/rushrLogo.png'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import ncrBranding from '../assets/ncrBranding.png'
+
+const Sidebar = () => {
+    return(
+        <div className={"sidebar-wrapper"}>
+            <div className={"rushr-logo"}>
+                <img src={rushrLogo}
+                     alt={"rushr"}
+                     style={{width: "50%", height: "50%"}} />
+            </div>
+            <div className={"credentials-form"} style={{"margin-top": "10%"}}>
+                <Form>
+                    <Form.Group controlId="formEmail">
+                        <Form.Control size="lg" type="email" placeholder="Email" />
+                    </Form.Group>
+                    <Form.Group controlId="formPassword">
+                        <Form.Control size="lg" type="password" placeholder="Password" style={{"margin-top": "25px"}} />
+                    </Form.Group>
+                    <div style={{"text-align": "right"}}>
+                        <Form.Label style={{"font-size": "15px", "font-weight": "bold", color: "#66C15D"}}>
+                            Forgot your password?
+                        </Form.Label>
+                    </div>
+                    <br/>
+                    <Button size="lg" variant="success" type="submit" style={{"width": "50%"}}>
+                        Sign in
+                    </Button>
+                </Form>
+            </div>
+            <div className={"new-acct"}>
+                New to rushr? <b style={{"color": "#66C15D"}}> Create an account </b>
+            </div>
+            <div style={{"text-align": "center"}}>
+            <img src={ncrBranding}
+                 alt={""}
+                 style={{"width": "30%", "height": "auto", "margin-top": "90%"}}
+                 />
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
